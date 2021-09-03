22ca3e927a245259d7a54220d6133893707d98e8
Giorgi Chachanidze <chachanidze.grg@gmail.com>
2021-09-03 14:32:20 +0400

    Initial commit

---
+++ GenreButtonScreen.swift
@@ -0,0 +1,51 @@
+//
+//  GenreButtonScreen.swift
+//  MusicPlayer
+//
+//  Created by Giorgi Chachanidze on 9/3/21.
+//
+
+import UIKit
+import MediaPlayer
+
+class GenreButtonScreen: UIViewController {
+
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func genreButtonTapped(_ sender: UIButton) {
+        MPMediaLibrary.requestAuthorization { (status) in
+            if status == .authorized {
+                DispatchQueue.main.async {
+                              self.playGenre(genre: sender.currentTitle!)
+                            }
+            }
+        }
+        
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        musicPlayer.stop()
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: Any) {
+        musicPlayer.skipToNextItem()
+    }
+    
+    func playGenre(genre: String) {
+        
+        musicPlayer.stop()
+        
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value:genre, forProperty: MPMediaItemPropertyGenre)
+        query.addFilterPredicate(predicate)
+        
+        musicPlayer.setQueue(with: query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
+    }
+}
